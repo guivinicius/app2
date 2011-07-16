bf3b7594cb26a6507315284a374333612eb54f2c
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2011-07-16 14:56:11 -0300

    Commit inicial

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc4'
+
+# Bundle edge Rails instead:
+# gem 'rails',     :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+gem 'jquery-rails'
+
+group :test, :development do
+	gem 'turn', :require => false
+  	gem "rspec-rails", "~> 2.4"
+	gem "capybara"
+	gem 'simplecov', :require => false
+	gem "shoulda-matchers"
+	gem 'factory_girl_rails'
+	gem "ruby-debug19"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App2::Application.config.secret_token = '7c67ed7f0c6ec11e8aac1f132152015f5021504df29a493ea3e605e9b0e920dbda87d1ea226141a4c0f530430fcdb60b73d9a8523383c2ce919b8f405ab1beba'
